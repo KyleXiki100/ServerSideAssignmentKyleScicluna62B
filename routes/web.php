2448465d060b4f4324a423678d528308bb1fe5f4
Kyle <kylexiki100@gmail.com>
2025-02-26 12:21:20 +0000

    Added Routes and Controller to Collage And Student

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CollegeController;
+use App\Http\Controllers\StudentController;
+
+
+Route::resource('colleges', CollegeController::class);
+Route::resource('students', StudentController::class);
+
 
 Route::get('/', function () {
     return view('welcome');
