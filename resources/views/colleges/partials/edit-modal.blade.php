572d9766c090674bcaeb5fe2f6c775efd21fe004
Kyle <kylexiki100@gmail.com>
2025-04-01 13:22:14 +0200

    Updated Validation for College index

--- resources/views/colleges/partials/edit-modal.blade.php
+++ resources/views/colleges/partials/edit-modal.blade.php
@@ -1,43 +1,56 @@
-<div 
-    class="modal fade" 
-    id="editCollegeModal-{{ $college->id }}" 
-    tabindex="-1" 
-    role="dialog" 
-    aria-labelledby="editCollegeModalLabel-{{ $college->id }}" 
-    aria-hidden="true"
->
+<div class="modal fade" id="editCollegeModal-{{ $college->id }}" tabindex="-1" role="dialog" aria-labelledby="editCollegeModalLabel-{{ $college->id }}" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <form action="{{ route('colleges.update', $college->id) }}" method="POST">
       @csrf
       @method('PUT')
       <div class="modal-content">
-      
-       
         <div class="modal-header">
           <h5 class="modal-title" id="editCollegeModalLabel-{{ $college->id }}">
             Edit College #{{ $college->id }}
           </h5>
-          <button 
-              type="button" 
-              class="close" 
-              data-dismiss="modal" 
-              aria-label="Close"
-          >
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
         
-        
         <div class="modal-body">
-          @include('colleges.partials.form-fields', ['college' => $college])
+          <div class="form-group">
+            <label for="name-{{ $college->id }}">College Name</label>
+            <input 
+              type="text" 
+              name="name" 
+              id="name-{{ $college->id }}" 
+              class="form-control @error('name') is-invalid @enderror" 
+              value="{{ old('name', $college->name) }}" 
+              required
+            >
+            @error('name')
+              <div class="invalid-feedback">
+                {{ $message }}
+              </div>
+            @enderror
+          </div>
+
+          <div class="form-group">
+            <label for="address-{{ $college->id }}">Address</label>
+            <textarea 
+              name="address" 
+              id="address-{{ $college->id }}" 
+              class="form-control @error('address') is-invalid @enderror" 
+              required
+            >{{ old('address', $college->address) }}</textarea>
+            @error('address')
+              <div class="invalid-feedback">
+                {{ $message }}
+              </div>
+            @enderror
+          </div>
         </div>
-        
         
         <div class="modal-footer">
           <button type="submit" class="btn btn-primary">Update College</button>
           <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
         </div>
-        
       </div>
     </form>
   </div>
--- resources/views/colleges/partials/form-fields.blade.php
+++ resources/views/colleges/partials/form-fields.blade.php
@@ -1,23 +1,54 @@
-<div class="form-group">
-    <label for="name">College Name</label>
-    <input 
-        type="text"
-        name="name"
-        id="name"
-        class="form-control"
-        value="{{ old('name', $college->name ?? '') }}"
-        required
-    >
-</div>
+<div class="modal fade" id="createCollegeModal" tabindex="-1" role="dialog" aria-labelledby="createCollegeModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <form action="{{ route('colleges.store') }}" method="POST">
+      @csrf
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="createCollegeModalLabel">Add New College</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        
+        <div class="modal-body">
+          <div class="form-group">
+            <label for="name">College Name</label>
+            <input 
+              type="text" 
+              name="name" 
+              id="name" 
+              class="form-control @error('name') is-invalid @enderror" 
+              value="{{ old('name') }}" 
+              required
+            >
+            @error('name')
+              <div class="invalid-feedback">
+                {{ $message }}
+              </div>
+            @enderror
+          </div>
 
-<div class="form-group">
-    <label for="address">Address</label>
-    <input 
-        type="text"
-        name="address"
-        id="address"
-        class="form-control"
-        value="{{ old('address', $college->address ?? '') }}"
-        required
-    >
+          <div class="form-group">
+            <label for="address">Address</label>
+            <textarea 
+              name="address" 
+              id="address" 
+              class="form-control @error('address') is-invalid @enderror" 
+              required
+            >{{ old('address') }}</textarea>
+            @error('address')
+              <div class="invalid-feedback">
+                {{ $message }}
+              </div>
+            @enderror
+          </div>
+        </div>
+        
+        <div class="modal-footer">
+          <button type="submit" class="btn btn-primary">Add College</button>
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+        </div>
+      </div>
+    </form>
+  </div>
 </div>
